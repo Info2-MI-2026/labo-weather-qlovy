95efb265ba57a1734b89c9a5409376b034452a48
Quentin Lovy <quentin.lovy@hes-so.ch>
2026-03-19 09:39:40 +0100

    format code

--- weather-analyser.c
+++ weather-analyser.c
@@ -40,15 +40,13 @@ typedef struct
     bool binary_output;
 } Options;
 
-void version(FILE *fp)
-{
+void version(FILE* fp) {
     fprintf(fp,
             "Version 0.0.1 "
             "Copyright(c) HEIG-VD\n");
 }
 
-void help(FILE *fp)
-{
+void help(FILE* fp) {
     fprintf(fp,
             "USAGE: ./weather-analyser [options] [filename]\n\n"
             "This program processes weather data issues from the internet. \n"
@@ -62,13 +60,13 @@ void help(FILE *fp)
             "    -o<filename>   Write output on <filename>\n\n");
 }
 
-int collect_data(WData *data, FILE *fp) {
+int collect_data(WData* data, FILE* fp) {
     char txt[MAX_LINE];
-    while(fgets(txt, MAX_LINE, fp) != NULL){
+    while (fgets(txt, MAX_LINE, fp) != NULL) {
         int n1, n2;
         double d1, d2;
-        if (sscanf(txt, "%d\t%d\t%lf\t%lf", &n1, &n2, &d1, &d2) == 4){
-            if (data->start == 0){
+        if (sscanf(txt, "%d\t%d\t%lf\t%lf", &n1, &n2, &d1, &d2) == 4) {
+            if (data->start == 0) {
                 data->start = n1;
             }
             data->years[n1 - data->start].year = n1;
@@ -79,32 +77,33 @@ int collect_data(WData *data, FILE *fp) {
     return 0;
 }
 
-void process_data(WData *data) {
-    for (int i=0 ; i<MAX_ENTRIES; i++){
+void process_data(WData* data) {
+    for (int i = 0; i < MAX_ENTRIES; i++) {
         double sum_temp = 0;
         double sum_precip = 0;
-        for (int j=0; j<MONTHS; j++){
+        for (int j = 0; j < MONTHS; j++) {
             sum_temp += data->years[i].months[j].temperature;
             sum_precip += data->years[i].months[j].precipitations;
         }
-        data->years[i].temperature = sum_temp/MONTHS;
+        data->years[i].temperature = sum_temp / MONTHS;
         data->years[i].precipitations = sum_precip;
     }
 }
 
-void fprint_csv(FILE *fp, WData *data) {
+void fprint_csv(FILE* fp, WData* data) {
     fprintf(fp, "year;temperatures;precipitations\n");
-    for (int i=0; i<MAX_ENTRIES; i++){
+    for (int i = 0; i < MAX_ENTRIES; i++) {
         if (data->years[i].year == 0) break;
-        fprintf(fp, "%d,%.1lf,%.1lf\n", data->years[i].year, data->years[i].temperature, data->years[i].precipitations);
+        fprintf(fp, "%d,%.1lf,%.1lf\n", data->years[i].year, data->years[i].temperature,
+                data->years[i].precipitations);
     }
 }
 
-void fprint_binary(FILE *fp, WData *data) {
+void fprint_binary(FILE* fp, WData* data) {
     char* header = "WEATHER";
     const int size = sizeof(header);
     fwrite(header, size, 1, fp);
-    for(int i=0; i<MAX_ENTRIES; i++){
+    for (int i = 0; i < MAX_ENTRIES; i++) {
         if (data->years[i].year == 0) break;
         fwrite(&(data->years[i].year), 4, 1, fp);
         int temp = data->years[i].temperature * 10;
@@ -114,33 +113,32 @@ void fprint_binary(FILE *fp, WData *data) {
     }
 }
 
-void process_arg(int argc, char *argv[], Options* options)
-{
-    for (int i = 1; i < argc; i++){
-        if (strcmp(argv[i], "--version") == 0){
+void process_arg(int argc, char* argv[], Options* options) {
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "--version") == 0) {
             version(stdout);
-        }else if (strcmp(argv[i], "--help") == 0){
+        } else if (strcmp(argv[i], "--help") == 0) {
             help(stdout);
-        }else if (strcmp(argv[i], "--binary") == 0 || strcmp(argv[i], "-b") == 0){
+        } else if (strcmp(argv[i], "--binary") == 0 || strcmp(argv[i], "-b") == 0) {
             options->binary_output = true;
-        }else if(sscanf(argv[i], "-o%s", options->out_filename_csv) == 1){
-        }else{
+        } else if (sscanf(argv[i], "-o%s", options->out_filename_csv) == 1) {
+        } else {
             sscanf(argv[i], "%s", options->in_filename);
         }
     }
-    if (options->binary_output){
+    if (options->binary_output) {
         strcpy(options->out_filename_bin, options->out_filename_csv);
     }
 }
 
-int main(int argc, char *argv[])
-{
-    Options options = {.in_filename = "assets/weather-bern.txt", 
-        .out_filename_csv = "out.csv", .out_filename_bin = "out.bin",
-        .binary_output = false};
+int main(int argc, char* argv[]) {
+    Options options = {.in_filename = "assets/weather-bern.txt",
+                       .out_filename_csv = "out.csv",
+                       .out_filename_bin = "out.bin",
+                       .binary_output = false};
 
     process_arg(argc, argv, &options);
-    
+
     WData wdata1 = {0};
     FILE* f = fopen(options.in_filename, "r");
     if (f == NULL) return 1;
@@ -148,12 +146,12 @@ int main(int argc, char *argv[])
     fclose(f);
     process_data(&wdata1);
 
-    if (options.binary_output){
+    if (options.binary_output) {
         f = fopen(options.out_filename_bin, "w");
         if (f == NULL) return 2;
         fprint_binary(f, &wdata1);
         fclose(f);
-    }else{
+    } else {
         f = fopen(options.out_filename_csv, "w");
         if (f == NULL) return 3;
         fprint_csv(f, &wdata1);
